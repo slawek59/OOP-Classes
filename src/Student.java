6acb5ece344a595096df1908cd72b1a88087ffd3
Sławomir Wąs <117323857+slawek59@users.noreply.github.com>
2025-03-09 13:29:19 +0100

    add constructor to Student.java

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,11 @@ public class Student {
     private String name;
     private double grade;
 
+    public Student(string name, double grade){
+        this.name = name;
+        this.grade = grade;
+    }   
+    
     public boolean hasPassed(){
         return grade >= 5.0;
     }
